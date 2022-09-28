851876fadb64e1c8ce302531cbf43234c52ad281
Sam Marston <smarston@easypost.com>
2022-09-27 22:33:21 -0700

    organized rates and rate errors on the rates page

--- app.py
+++ app.py
@@ -19,17 +19,16 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+
 # handle form data
 @app.route('/submit', methods=['POST'])
 def submit():
     if request.method == 'POST':
-        # """ SET TEST OR PROD API KEY """
-        # if request.form['api_key_type'] == "Production":
-        #     easypost.api_key = prod_key 
-        # elif request.form['api_key_type'] == "Test":
-        easypost.api_key = test_key
-        print(easypost.api_key)
 
+        # set the API key in the client library
+        easypost.api_key = test_key
+
+        # create shipment dictionary using info sent from the client
         ship = {
             "to_address": {
                 "name": request.form['to_address_name'],
@@ -59,17 +58,19 @@ def submit():
             },
         }
 
+        # check to see if street2 was provided
         if request.form['to_address_street2'] != '':
             ship['to_address']['street2'] = request.form['to_address_street2']
         else:
             ship['to_address']['street2'] = None
         
+        # check if to_address.residential is true
         if request.form['to_address_residential'] == "true":
             ship['to_address']['residential'] = True
         else:
             ship['to_address']['residential'] = False
 
-        # print(request.form['parcel_predefined_package'])
+        # remove provided dims if a predefined_package was included
         if request.form['parcel_predefined_package'] != '':
             ship['parcel']['length'] = None
             ship['parcel']['height'] = None
@@ -78,19 +79,9 @@ def submit():
         else:
             ship['parcel']['predefined_package'] = None
 
-
-            
-
-        
-
-        # print(json.dumps(ship, indent=4)) 
-        
-        # shipment = easypost.Shipment.create(ship)
-        # print(shipment)
-
-        # return render_template('rates.html', rate_error_message=shipment.messages,rates=shipment.rates)  
-        
+        # create the shipment and fetch rates from EasyPost
         try: 
+            print('hit try block')
             print(json.dumps(ship, indent=4)) 
             
             shipment = easypost.Shipment.create(
@@ -100,11 +91,19 @@ def submit():
             )
             print(shipment)
 
+            # sort rates by `carrier` before sending them to the client
+            def sorted_rates(e):
+                return e['carrier']
+
+            shipment.rates.sort(key=sorted_rates)
+
+            # if the above is successful, render the rates.html template
             return render_template('rates.html', rate_error_message=shipment.messages,rates=shipment.rates)  
 
-        except easypost.error.Error as e:
-            print(e.http_body)
-            return render_template('index.html', error=f"Oops! There was a problem: {e.http_body}")
+        except Exception as e:
+            print(e)
+            # render the index.html template but with an alert at the top with the Exception
+            return render_template('index.html', error=f"Oops! There was a problem: {e}")
 
 
 
